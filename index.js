51d13612356e82de135bcdcd2475d083907f098c
Munish Kumar <mukumar@EX2INDIA.LOCAL>
2022-02-02 22:15:52 +0530

    Final Commit

--- index.js
+++ index.js
@@ -1,36 +1,60 @@
 function handleNames(names){
     //It means that the comma is not present in the string...
     let array = names.split(' ');
-    let str = ''
+    let str = '';
+    //Array for storing the lowercase words..
     let lowercase = [];
+    //Array for storing the uppercase words....
     let uppercase = [];
-    for(let i=0;i<array.length;i++){
-        //If the string is asscoiated with commas then remove the comma 
-        let x = array[i].search(',')==-1?array[i]:array[i].substr(0, array[i].length-1);
-        //store the uppercase somewhere..
-        let x2 = x.toUpperCase();
-        //if the string is uppercase then push into uppercase array..
-        if(x == x2){
-            uppercase.push(x);
+    for(let i=0;i<array.length;){
+        //It means that there is a starting of one single unit....
+        if(array[i][0] == '"'){
+            //Initialized empty string for storing the single unit...
+            let string = '';
+            let j=i;
+            //Iteration for storing the words of one unit in the particular string...
+            for(;array[j][array[j].length-1] !== '"' && j<array.length;j++){
+                string+=array[j]+' ';
+            }
+            //Storing the last word...
+            string+=array[j];
+            //Storing the final one unit string in the 'local' variable..
+            let local = string.substr(1, string.length-2);
+            //Pushing into the lowercase array by default...
+            lowercase.push(local);
+            //As the single unit is completed so the i pointer is incremented directly to the new word..
+            i = j+1;
         }
-        //else push into the lowercase array..
         else{
-            lowercase.push(x);
+            //If the string is asscoiated with commas then remove the comma 
+            let x = array[i].search(',')==-1?array[i]:array[i].substring(0, array[i].length-1);
+            //store the uppercase somewhere..
+            let x2 = x.toUpperCase();
+            //if the string is uppercase then push into uppercase array..
+            if(x == x2){
+                uppercase.push(x);
+            }
+            //else push into the lowercase array..
+            else{
+                lowercase.push(x);
+            }
+            //To new word...
+            i++;
         }
     }
     //If the name is only one...
-    if(array.length == 1){
+    if(lowercase.length + uppercase.length == 1){
         //If there is only uppercase..
         if(lowercase.length == 0){
-            str = `HELLO ${names}!`;
+            str = `HELLO ${uppercase[0]}!`;
             return str;
         }
         //else we shall handle the lowercase..
-        str = `Hello ${names}!`
+        str = `Hello ${lowercase[0]}!`
         return str;
     }
     //If there are two names in the array....
-    if(array.length == 2){
+    if(lowercase.length + uppercase.length == 2){
         //If all names are lowercase..
         if(uppercase.length == 0){
             str = `Hello, ${lowercase[0]} and ${lowercase[1]}.`
@@ -52,7 +76,7 @@ function handleNames(names){
         for(let i=0;i<uppercase.length-1;i++){
             str+=`${uppercase[i]}, `
         }
-        str+=`and ${uppercase[uppercase.length-1]}.`
+        str+=`AND ${uppercase[uppercase.length-1]}.`
         return str;
     }
     //If all the names are of lowercase...
@@ -74,7 +98,7 @@ function handleNames(names){
     for(let i=0;i<uppercase.length-1;i++){
         str+=`${uppercase[i]}, `
     }
-    str+=(uppercase.length > 1)?` AND ${uppercase[uppercase.length-1]}!`:`${uppercase[uppercase.length-1]}!`
+    str+=(uppercase.length > 1)?`AND ${uppercase[uppercase.length-1]}!`:`${uppercase[uppercase.length-1]}!`
     return str;
 }
 
