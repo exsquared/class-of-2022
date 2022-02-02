364d3a93050591ccaa67c55a2e181b32f245cc20
Fardeen Khan <fkhan@EX2INDIA.LOCAL>
2022-02-02 21:32:11 +0530

    2022/day-two-fkhan_Updated

--- main.js
+++ main.js
@@ -25,50 +25,57 @@ function combine(array) {
    
     let small = "Hello, ", smallAns = "Hello, ";
     let caps = "HELLO ", capAns = "HELLO ";
-    
-    let condition = false;
-    
     let smallName=[];
     let capName=[];
-    for(let i=0; i<array.length; i++){
-        if(condition){
-            condition = false;
-            continue;
-        }
-        // Dividing the contents of the string array to different classes so can be handled accordingly
-        if(array[i] == array[i].toUpperCase() && array[i].includes(",") && array[i][0] === "\""){   // Requirement 8
-            capName.push(array[i].substring(1)+" "+array[i+1].subString(0,array[i+1].length-1));
-            condition = true;
-        }else if(array[i] == array[i].toLowerCase() && array[i].includes(",") && array[i][0] === "\""){  // Requirement 8
-            smallName.push(array[i].substring(1)+" "+array[i+1].substring(0,array[i+1].length-1));
-            condition = true;
-        }else if(array[i] == array[i].toUpperCase() && array[i].includes(",")){  // Requirement 7
-            capName.push(array[i].substring(0,array[i].length-1));
-        }else if(array[i] == array[i].toLowerCase() && array[i].includes(",")){  // Requirement 7
-            smallName.push(array[i].substring(0, array[i].length-1));
-        }else if(array[i] === array[i].toUpperCase()){ // Requirement 6
+    for(let i=0; i<array.length; i++){    // Separating Normal Greetings and Shouting Greetings
+        if(array[i] === array[i].toUpperCase()){ 
             capName.push(array[i]);
         }else{
             smallName.push(array[i]);
         }
     }
-    for(const name of smallName){
-        if(smallName.indexOf(name) === smallName.length-1){  // as last element end's with a full stop
-            smallAns += name+".";
-        }else if(smallName.indexOf(name) === smallName.length-2){ // as second last element ends with 'and'
-            smallAns += name + " and ";
+    
+    for(let j=0; j<smallName.length; j++){
+        let str = smallName[j];
+        if(str.includes(",") && str[0] === "\""){   //Handling Requirement 8
+            str = str.substring(1,str.length);
+            while(smallName[j+1][smallName[j+1].length -1] !== "\""){
+                str += " "+smallName[j+1];
+                j++;
+            }
+            str += " "+smallName[++j].substring(0,smallName[j].length-1);
+        }else if(str.includes(",")){     // Handling Requirement 7
+            str = str.substring(0,str.length-1);
+        }
+        if(j === smallName.length-1){   // Handling Last Person's Greetings
+            smallAns += "and "+str+".";
         }else{
-            smallAns += name+", ";  // all intermediate elements are followed by a comma ','
+            smallAns += str+", ";    //Handling Intermediate Person's Greetings
         }
     }
-    for(const name of capName){
-        if(capName.indexOf(name) === capName.length-1){   // as last element end's with a '!'
-            capAns += name+"!";
-        }else if(capName.indexOf(name) === capName.length-2){   // as second last element ends with 'and'
-            capAns += name + " AND ";
-        }else{               // all intermediate elements are followed by a comma ','
-            capAns += name+", ";
+    
+    for(let j=0; j<capName.length; j++){
+        let str = capName[j];
+        if(str.includes(",") && str[0] === "\""){    //Handling Requirement 8
+            str = str.substring(1,str.length);
+            while(capName[j+1][capName[j+1].length -1] !== "\""){
+                str += " "+capName[j+1];
+                j++;
+            }
+            str += " "+capName[++j].substring(0,capName[j].length-1);
+        }else if(str.includes(",")){       // Handling Requirement 7
+            str = str.substring(0,str.length-1);
         }
+        if(capName.length === 1){   // Handling Single Shouting Greeting
+            capAns += str+"!";
+            break;
+        }else{
+            if(j === capName.length-1){   // Handling Last Person's Shouting Greetings
+                capAns += "AND "+str+"!";
+            }else{
+                capAns += str+", ";    //Handling Intermediate Person's Shouting Greetings
+            }
+        }  
     }
         
     if(caps === capAns){   // checking whether there is no shouting
