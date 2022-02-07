a3fced4ebc098314fbf1612ead7cb7ee0703de13
Lalit Singh <lsingh@EX2INDIA.LOCAL>
2022-02-07 23:10:22 +0530

    Updated TDD cases

--- index.js
+++ index.js
@@ -4,19 +4,23 @@ const free_minutes_for_gold_plan = 1000;
 const free_minutes_for_silver_plan = 500;
 
 export function calculateBill(plan, minutes=0, number_of_lines=1) {
-    if(number_of_lines == 0) {
+    if(number_of_lines <= 0) {
         return 0;
     }
     let base_amt = basePlanAmt(plan);
     return base_amt == 0 ? 0 : Math.round(finalPrice(plan, number_of_lines, base_amt, minutes)*100)/100;
 }
-function finalPrice(plan, number_of_lines, base_amt, minutes) {
+export function finalPrice(plan, number_of_lines=1, base_amt, minutes=0) {
     if(plan == "Gold") {
         return base_amt + (number_of_lines - 1) * 14.50 + extraChargesForMinutes(plan, minutes) - discountGiven(plan, number_of_lines);
-    }
+    } else if(plan=="Silver") {
     return base_amt + (number_of_lines - 1) * 21.50 + extraChargesForMinutes(plan, minutes) - discountGiven(plan, number_of_lines);
+    }
+    else {
+        return 0;
+    }
 }
-function basePlanAmt(plan) {
+export function basePlanAmt(plan) {
     if(plan == "Gold") {
         return gold_plan_base_price;
     } else if (plan == "Silver") {
@@ -25,13 +29,17 @@ function basePlanAmt(plan) {
         return 0;
     }            
 }
-function extraChargesForMinutes(plan,minutes) {
+export function extraChargesForMinutes(plan="Gold",minutes=0) {
     if (plan == "Gold") {
         return minutes > free_minutes_for_gold_plan ? (minutes - free_minutes_for_gold_plan) * 0.45 : 0;
+    } else if(plan=="Silver") {
+    return minutes > free_minutes_for_silver_plan ? (minutes - free_minutes_for_silver_plan) * 0.54 : 0;  
     }
-    return minutes > free_minutes_for_silver_plan ? (minutes - free_minutes_for_silver_plan) * 0.54 : 0;    
+    else {
+        return 0;
+    }  
 }
-function discountGiven(plan, number_of_lines) {
+export function discountGiven(plan="Gold", number_of_lines=1) {
     if (number_of_lines > 3) {
         return plan == "Gold" ? (number_of_lines - 3) * 9.50 : (number_of_lines - 3) * 16.50; 
     } 
--- index.test.js
+++ index.test.js
@@ -1,125 +1,213 @@
-import { calculateBill } from './index';
+import { calculateBill, finalPrice, basePlanAmt, extraChargesForMinutes, discountGiven } from './index';
 
 
-describe("The function should return the phone bill according to plan type and number of lines", () =>
-{
-    it("Should return if null is passed.", () =>
-    {
+describe("index.js", () => {   
+    describe("calculateBill", () => {
+        it("1. Should return 0 if arguments are missing.", () => {
+            const Expected_Bill=calculateBill();
+            expect(Expected_Bill).toBe(0);
+        });
+        it("2. Should return if null is passed.", () => {
         const plan=null;
         const lines=null;
         const Expected_Bill=calculateBill(plan,0,lines);
         expect(Expected_Bill).toEqual(0);
-    });
-    it("Should return 49.95 if plan is gold and number of lines is 1", () =>
-    {
+        });
+        it("3. Should return 49.95 if plan is gold and number of lines is 1", () => {
         const plan="Gold";
         const lines=0;
         const Expected_Bill=calculateBill(plan,0,0);
         expect(Expected_Bill).toEqual(0);
-    });
-    it("Should return 49.95 if plan is Silver and number of lines is 1", () =>
-    {
+        });
+        it("4. Should return 29.95 if plan is Silver and number of lines is 1", () => {
         const plan="Silver";
         const lines=1;
         const Expected_Bill=calculateBill(plan,0,lines);
         expect(Expected_Bill).toEqual(29.95);
-    });
-    it("Should return 49.95 if plan is gold and number of lines is 2", () =>
-    {
+        });
+        it("5. Should return 64.45 if plan is gold and number of lines is 2", () => {
         const plan="Gold";
         const lines=2;
         const Expected_Bill=calculateBill(plan,0,lines);
         expect(Expected_Bill).toBe(64.45);
-    });
-    it("Should return 49.95 if plan is Silver and number of lines is 2", () =>
-    {
+        });
+        it("6. Should return 51.45 if plan is Silver and number of lines is 2", () => {
         const plan="Silver";
         const lines=2;
         const Expected_Bill=calculateBill(plan,0,lines);
         expect(Expected_Bill).toBe(51.45);
-    });
-    it("Should return the bill amount.", () => {
-        const inputPlan = "Gold";
-        const billAmount = calculateBill(inputPlan);
-        expect(billAmount).toBe(49.95);
-    });
-    it("Should return the bill amount.", () => {
-        const inputPlan = "Silver";
-        const billAmount = calculateBill(inputPlan);
-        expect(billAmount).toBe(29.95);
-    });
-    it("Should return the bill amount.", () => {
-        const inputPlan = "Platinum";
-        const billAmount = calculateBill(inputPlan);
-        expect(billAmount).toBe(0);
-    });
-    it("Should return the bill amount.", () => {
-        const inputPlan = "Gold";
+        });
+        it("7. Should return the bill amount.", () => {
+        const plan = "Gold";
+        const Expected_Bill = calculateBill(plan);
+        expect(Expected_Bill).toBe(49.95);
+        });
+        it("8. Should return the bill amount.", () => {
+        const plan = "Silver";
+        const Expected_Bill = calculateBill(plan);
+        expect(Expected_Bill).toBe(29.95);
+        });
+        it("9. Should return the bill amount.", () => {
+        const plan = "Platinum";
+        const Expected_Bill = calculateBill(plan);
+        expect(Expected_Bill).toBe(0);
+        });
+        it("10. Should return the bill amount.", () => {
+        const plan = "Gold";
         const totalLines = 2;
-        const billAmount = calculateBill(inputPlan, 0, totalLines);
-        expect(billAmount).toBe(64.45);
-    });
-    it("Should return the bill amount.", () => {
-        const inputPlan = "Gold";
+        const Expected_Bill = calculateBill(plan, 0, totalLines);
+        expect(Expected_Bill).toBe(64.45);
+        });
+        it("11. Should return the bill amount.", () => {
+        const plan = "Gold";
         const totalLines = 3;
-        const billAmount = calculateBill(inputPlan, 0, totalLines);
-        expect(billAmount).toBe(78.95);
-    });
-    it("Should return the bill amount.", () => {
-        const inputPlan = "Silver";
+        const Expected_Bill = calculateBill(plan, 0, totalLines);
+        expect(Expected_Bill).toBe(78.95);
+        });
+        it("12. Should return the bill amount.", () => {
+        const plan = "Silver";
         const totalLines = 3;
-        const billAmount = calculateBill(inputPlan, 0, totalLines);
-        expect(billAmount).toBe(72.95);
-    });
-    it("Should return the bill amount.", () => {
-        const inputPlan = "Gold";
+        const Expected_Bill = calculateBill(plan, 0, totalLines);
+        expect(Expected_Bill).toBe(72.95);
+        });
+        it("13. Should return the bill amount.", () => {
+        const plan = "Gold";
         const totalMinutes = 1001;
-        const billAmount = calculateBill(inputPlan, totalMinutes);
-        expect(billAmount).toBe(50.40);
-    });
-    it("Should return the bill amount.", () => {
-        const inputPlan = "Gold";
+        const Expected_Bill = calculateBill(plan, totalMinutes);
+        expect(Expected_Bill).toBe(50.40);
+        });
+        it("14. Should return the bill amount.", () => {
+        const plan = "Gold";
         const totalMinutes = 1002;
-        const billAmount = calculateBill(inputPlan, totalMinutes);
-        expect(billAmount).toBe(50.85);
-    });
-    it("Should return the bill amount.", () => {
-        const inputPlan = "Gold";
+        const Expected_Bill = calculateBill(plan, totalMinutes);
+        expect(Expected_Bill).toBe(50.85);
+        });
+        it("15. Should return the bill amount.", () => {
+        const plan = "Gold";
         const totalMinutes = 999;
-        const billAmount = calculateBill(inputPlan, totalMinutes);
-        expect(billAmount).toBe(49.95);
-    });
-    it("Should return the bill amount.", () => {
-        const inputPlan = "Gold";
+        const Expected_Bill = calculateBill(plan, totalMinutes);
+        expect(Expected_Bill).toBe(49.95);
+        });
+        it("16. Should return the bill amount.", () => {
+        const plan = "Gold";
         const totalMinutes = 999;
-        const billAmount = calculateBill(inputPlan, totalMinutes);
-        expect(billAmount).toBe(49.95);
-    });
-    it("Should return the bill amount.", () => {
-        const inputPlan = "Gold";
+        const Expected_Bill = calculateBill(plan, totalMinutes);
+        expect(Expected_Bill).toBe(49.95);
+        });
+        it("17. Should return the bill amount.", () => {
+        const plan = "Gold";
         const totalMinutes = 999;
         const totalLines = 4;
-        const billAmount = calculateBill(inputPlan, totalMinutes, totalLines);
-        expect(billAmount).toBe(83.95);
-    });
-    it("Should return the bill amount.", () => {
-        const inputPlan = "Silver";
+        const Expected_Bill = calculateBill(plan, totalMinutes, totalLines);
+        expect(Expected_Bill).toBe(83.95);
+        });
+        it("18. Should return the bill amount.", () => {
+        const plan = "Silver";
         const totalMinutes = 499;
         const totalLines = 4;
-        const billAmount = calculateBill(inputPlan, totalMinutes, totalLines);
-        expect(billAmount).toBe(77.95);
-    });
-    it("Should return the bill amount.", () => {
-        const inputPlan = "Silver";
+        const Expected_Bill = calculateBill(plan, totalMinutes, totalLines);
+        expect(Expected_Bill).toBe(77.95);
+        });
+        it("19. Should return the bill amount.", () => {
+        const plan = "Silver";
         const totalMinutes = 499;
         const totalLines = 5;
-        const billAmount = calculateBill(inputPlan, totalMinutes, totalLines);
-        expect(billAmount).toBe(82.95);
-    });
-    it("Should return the bill amount.", () => {
-        const inputPlan = "Silver";
+        const Expected_Bill = calculateBill(plan, totalMinutes, totalLines);
+        expect(Expected_Bill).toBe(82.95);
+        });
+        it("20. Should return the bill amount.", () => {
+        const plan = "Silver";
         const totalMinutes = 1076;
-        const billAmount = calculateBill(inputPlan, totalMinutes);
-        expect(billAmount).toBe(340.99);
+        const Expected_Bill = calculateBill(plan, totalMinutes);
+        expect(Expected_Bill).toBe(340.99);
+        });
+    });
+    describe("finalPrice",() => {
+        it("1. should return 58.5", () => {
+            const Expected = finalPrice("Gold",4,20,1010);
+            expect(Expected).toBe(58.5);
+        });
+        it("2. should return 20", () => {
+            const Expected = finalPrice("Gold",0,30,1010);
+            expect(Expected).toBe(20);
+        });
+        it("3. should return 79", () => {
+            const Expected = finalPrice("Gold",10,15,578);
+            expect(Expected).toBe(79);
+        });
+        it("4. should return 1032", () => {
+            const Expected = finalPrice("Silver",18,50,2100);
+            expect(Expected).toBe(1032);
+        });
+        it("5. should return 73", () => {
+            const Expected = finalPrice("Silver",5,20,0);
+            expect(Expected).toBe(73);
+        });
+        it("6. should return 0", () => {
+            const Expected = finalPrice("");
+            expect(Expected).toBe(0);
+        });
+    });
+    describe("basePlanAmt",() => {
+        it("1. should return 0", () => {
+            const Expected=basePlanAmt("");
+            expect(Expected).toBe(0);
+        });
+        it("2. should return 49.95", () => {
+            const Expected=basePlanAmt("Gold");
+            expect(Expected).toBe(49.95);
+        });
+        it("3. should return 29.95", () => {
+            const Expected=basePlanAmt("Silver");
+            expect(Expected).toBe(29.95);
+        });
+        it("4. should return 0", () => {
+            const Expected=basePlanAmt("SomeString");
+            expect(Expected).toBe(0);
+        });
+    });
+    describe("extraChargesForMinutes", () => {
+        it("1.should return 0", () => {
+            const Expected=extraChargesForMinutes();
+            expect(Expected).toBe(0);  
+        });
+        it("2.should return 0", () => {
+            const Expected=extraChargesForMinutes("Gold",576);
+            expect(Expected).toBe(0);  
+        });
+        it("3.should return 198.18", () => {
+            const Expected=extraChargesForMinutes("Silver",867);
+            expect(Expected).toBe(198.18);  
+        });
+        it("4.should return 0", () => {
+            const Expected=extraChargesForMinutes("Gold",-123);
+            expect(Expected).toBe(0);  
+        });
+        it("5.should return ", () => {
+            const Expected=extraChargesForMinutes("Silver",-76);
+            expect(Expected).toBe(0);  
+        });
+    });
+    describe("discountGiven", () => {
+        it("1. should return 33", () => {
+            const Expected=discountGiven("Silver",5);
+            expect(Expected).toBe(33);  
+        });
+        it("2. should return 190", () => {
+            const Expected=discountGiven("Gold",23);
+            expect(Expected).toBe(190);  
+        });
+        it("3.should return 66", () => {
+            const Expected=discountGiven("",7);
+            expect(Expected).toBe(66);  
+        });
+        it("4.should return 0", () => {
+            const Expected=discountGiven("Gold");
+            expect(Expected).toBe(0);  
+        });
+        it("5.should return 0", () => {
+            const Expected=discountGiven();
+            expect(Expected).toBe(0);  
+        });
     });
 });
