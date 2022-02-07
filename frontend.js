9c73e6a965506a6a8906cd5277fbfce0373b25c9
Fardeen Khan <fkhan@ex2india.com>
2022-02-07 15:36:06 +0530

    updated

--- frontend.js
+++ frontend.js
@@ -8,28 +8,32 @@ const minutes = document.querySelector('#minutes_Used');
 const calculateBill = document.querySelector('#calculate_Bill');
 const outputfield = document.querySelector('#result_Box');
 
-calculateBill.addEventListener('click', getResult);
-
-function getResult(e){
-    e.preventDefault();
-    const userType = (itemGold.checked)? itemGold.value : itemSilver.value;
-    const userLines = parseInt(linesUsed.value) ;
-    const userMinutes = parseInt(minutes.value);
-
-    if(!itemGold.checked && !itemSilver.checked){
-        alert("Please select an item type.");
-    }else if(!userMinutes){
-        alert("Please enter the number of minutes used.");
-    }else{
-        const res = calculate(userType, userLines, userMinutes);
-    
-        if(res === "The selected item is not available for rent."){
-            outputfield.value = `${res}`;
-        }else{
-            outputfield.value = `${res}`;
-        }
-    }   
+for (let i = 1; i <= 6; i++) {
+    const options = document.createElement('option');
+    options.textContent = i;
+    options.value = i;
+    linesUsed.append(options);
 }
 
+calculateBill.addEventListener('click', getResult);
 
+function getResult(e) {
+    e.preventDefault();
+    const userType = (itemGold.checked) ? itemGold.value : itemSilver.value;
+    const userLines = parseInt(linesUsed.value);
+    const userMinutes = parseInt(minutes.value);
 
+    if (!itemGold.checked && !itemSilver.checked) {
+        alert("Please select an item type.");
+    } else if (!userMinutes) {
+        alert("Please enter the number of minutes used.");
+    } else {
+        const res = calculate(userType, userLines, userMinutes);
+
+        if (res === "The selected item is not available for rent.") {
+            outputfield.value = `${res}`;
+        } else {
+            outputfield.value = `${res}`;
+        }
+    }
+}
