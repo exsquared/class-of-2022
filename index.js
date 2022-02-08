e5c17ec7d90a7f12f0551df6be68e8e83944c06b
Munish-ex2 <mukumar@ex2india.com>
2022-02-08 10:15:16 +0530

    modifiedIfCondition

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import {calculateMoney} from './calculation.js'
 export function calculateBill(plan = '', numberOfLines = 1, numberOfMinutes = 0){
     //If number of Lines becomes less than 1 or number of minutes become negative....
     //If the number is passed as string..
-    if(typeof(numberOfLines) == 'string' || typeof(numberOfMinutes) == 'string' || numberOfMinutes < 0 || numberOfLines < 1)
+    if(isNaN(parseInt(numberOfLines)) || isNaN(parseInt(numberOfMinutes)) || parseInt(numberOfLines) < 1 || parseInt(numberOfMinutes) < 0)
         return 0;
     //If the execution reaches here, then number of lines and minutes are fine..
     //If the plan is not null, then proceed...
