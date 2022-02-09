3299ed1bccaf5e7714e7bb56c03860dfd3f304e5
Lalit Singh <lsingh@ex2india.com>
2022-02-09 10:26:30 +0530

    Update TDD cases

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let path = './data/cricket.txt';
+let path = './data/example.txt';
 function wordFrequencyCount(path) {
     let file = readFile(path);
     let text = preprocessFile(file);
@@ -15,13 +15,12 @@ function readFile(path) {
     if (isFilePresent) {
         file = fs.readFileSync(path).toString();
         return file;
-    } else {
-        return 0;
-    }
+    } 
+    return 0;
 }
 
 function preprocessFile(file) {
-    if (file == null || file == undefined || file == "" ) {
+    if (!file) {
         return 0;
     }
     let reg = /\W+/g;
@@ -30,9 +29,9 @@ function preprocessFile(file) {
 
 }
 function frequencyCounter(string) {
-    if (string == null || string == undefined || string == "" ) {
+    if (!string) {
     return 0;
-}
+    }
     let words = string.split(" ");
     let counter = {};
     for (let word of words) {
@@ -46,6 +45,9 @@ function frequencyCounter(string) {
 }
 
 function sortResultByCount(countedObject) {
+    if (!countedObject) {
+        return 0;
+    }
     let sortable = [];
     for (let word in countedObject) {
     sortable.push([word, countedObject[word]]);
@@ -53,16 +55,7 @@ function sortResultByCount(countedObject) {
     sortable.sort(function (a, b) {
         return a[1] - b[1];
     });
-    let objSorted = {}
-    sortable.forEach(function(item){
-    objSorted[item[0]]=item[1]
-    })
-    return objSorted;
+    return sortable;
 }
 
- /* let fish = wordFrequencyCount(path);
- let full = Object.keys(fish);
- console.log(full);
- console.log(typeof full[1]); */
-
-export { readFile, preprocessFile, frequencyCounter, sortResultByCount }
+export { readFile, preprocessFile, frequencyCounter, sortResultByCount, wordFrequencyCount };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-import { frequencyCounter, readFile, preprocessFile, sortResultByCount } from './index';
+import { frequencyCounter, readFile, preprocessFile, sortResultByCount, wordFrequencyCount } from './index';
 
 describe("index", () => {
     describe("filePath", () => {
@@ -34,23 +34,57 @@ describe("index", () => {
         });
     });
     describe("prepocessFile", () => {
-        it("it should return empty string if empty string id passed.", () => {
+        it("1.it should return 0 if empty string is passed.", () => {
             const Expected = preprocessFile("");
             expect(Expected).toEqual(0);
         });
-        it("it should return empty string without special characters.", () => {
+        it("2.it should return string without special characters.", () => {
             const Expected = preprocessFile("good-morning world!");
             expect(Expected).toEqual("good morning world");
         });
-        it("it should return empty string without special characters.", () => {
+        it("3. it should return string without special characters.", () => {
             const Expected = preprocessFile("This is a sentence with period.");
             expect(Expected).toEqual("this is a sentence with period");
         });
     });
     describe("sortResultByCount", () => {
-        it("should sort according to values.", () => {
+        it("1. should sort according to values.", () => {
             const Expected = sortResultByCount({hat:5, that:3, was:6, good:8, not:10});
-            expect(Expected).toEqual({ that: 3, hat: 5, was: 6, good: 8, not: 10 });
+            expect(Expected).toEqual( [
+                [ 'that', 3 ],
+                [ 'hat', 5 ],
+                [ 'was', 6 ],
+                [ 'good', 8 ],
+                [ 'not', 10 ]
+              ]);
         });
+    });
+    describe("wordFrequencyCount", () => {
+        it("1. should return 0 if no path is passed.", () => {
+            const Expected = wordFrequencyCount("");
+            expect(Expected).toEqual(0);
+        });
+        it("2. should return 0 if path is wrong.", () => {
+            const Expected = wordFrequencyCount("./data/incorrect.txt");
+            expect(Expected).toEqual(0);
+        });
+        it("3. should sort the words in file according to the count of words.", () => {
+            const Expected = wordFrequencyCount("./data/example.txt");
+            expect(Expected).toEqual([
+                [ 'hello', 1 ],
+                [ 'is', 1 ],
+                [ 'a', 1 ],
+                [ 'with', 1 ],
+                [ 'special', 1 ],
+                [ 'characters', 1 ],
+                [ 'and', 1 ],
+                [ 'has', 1 ],
+                [ 'some', 1 ],
+                [ 'repeated', 1 ],
+                [ 'words', 1 ],
+                [ 'this', 2 ],
+                [ 'string', 2 ]
+              ]);
+        });    
     });
 });
