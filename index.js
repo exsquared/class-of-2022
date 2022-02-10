dcb204ab6e1b5911d2ded86f6f1cc42bca2bc96a
Chandan Taneja <ctaneja@EX2INDIA.LOCAL>
2022-02-10 10:55:53 +0530

    merged with 2022/day-eight-ctaneja

---
+++ index.js
@@ -0,0 +1,25 @@
+import { preProcess } from './textProcessing.js';
+export { displayFrequencyOfEachWord, readFile };
+
+function displayFrequencyOfEachWord(path) {
+    const text = readFile(path);
+    if (text === -1) {
+        return 'file does not exists.';
+    }
+    else {
+        return preProcess(text);
+    }
+}
+
+
+function readFile(path) {
+    let fs = require('fs');
+    let text;
+    let isFilePresent = fs.existsSync(path);
+    if (isFilePresent) {
+        text = fs.readFileSync(path).toString();
+        return text;
+    } else {
+        return -1;
+    }
+}
---
+++ textProcessing.js
@@ -0,0 +1,36 @@
+export { preProcess };
+
+function preProcess(text) {
+    text = text.toLowerCase();
+    text = text.replace(/'s/g, '');
+    text = text.replace(/[^a-zA-Z ]/g, ' ');
+    text = text.replace(/\s+/g, ' ').trim();
+    if (text === '')
+        return '';
+
+    const words = text.split(' ');
+    const sortedFrequencyMap = createSortedFrequencyMapFromWords(words);
+    return createWordFrequencyPair(sortedFrequencyMap);
+}
+
+function createSortedFrequencyMapFromWords(words) {
+    let frequencyMap = new Map();
+    for (const word of words) {
+        if (frequencyMap.has(word)) {
+            frequencyMap.set(word, frequencyMap.get(word) + 1);
+        } else {
+            frequencyMap.set(word, 1);
+        }
+    }
+    let sortedFrequencyMap = new Map([...frequencyMap.entries()].sort((a, b) => b[1] - a[1]));
+    return sortedFrequencyMap;
+}
+
+function createWordFrequencyPair(sortedFrequencyMap) {
+    let sortedFrequencyString = '';
+    for (const [key, value] of sortedFrequencyMap.entries()) {
+        sortedFrequencyString += `${key}:${value} `;
+    }
+    sortedFrequencyString = sortedFrequencyString.slice(0, -1);
+    return sortedFrequencyString;
+}
