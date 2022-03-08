2b689d98231b44b08fb5f8aa43e1a9593d10c0bf
Fardeen Khan <fkhan@ex2india.com>
2022-03-08 10:22:51 +0530

    tdd implementation

--- index.js
+++ index.js
@@ -1,3 +1,116 @@
-function greet(){
+let normalGreets = [];
+let shoutedGreets = [];
+
+console.log(greet('Bob'));
+console.log(greet());
+console.log(greet("JERRY"));
+console.log(greet(["Jill","Jane"]));
+console.log(greet(["Amy", "Brian", "Charlotte"]));
+console.log(greet(["Amy", "BRIAN", "Charlotte"]));
+console.log(greet(["Bob", "Charlie, Dianne"]));
+console.log(greet(["Bob", "\"Charlie, Dianne\""]));
+
+export function greet(input){
+
+    fillArrays(input);
+    const res = normalGreetings()+shoutGreetings();
+    normalGreets = [];
+    shoutedGreets = [];
+    return res;
+}
+
+function normalGreetings(){
+    if(normalGreets.length === 0){
+        return '';
+    }
+    let res = "Hello, ";
+    if(normalGreets.length === 1){
+        return res+normalGreets[0]+'.';
+    }
+
+    if(normalGreets.length === 2){
+        return res + normalGreets[0] + " and " + normalGreets[1] + ".";
+    }
+
+    for(let i=0; i < normalGreets.length-1; i++){
+        res += normalGreets[i]+", ";
+    }
+    res += "and "+normalGreets[normalGreets.length-1]+".";
+    return res;
+}
+
+function shoutGreetings() {
+    if(shoutedGreets.length === 0){
+        return '';
+    }
+    let res = "HELLO ";
+    if(normalGreets.length !== 0){
+        res = " AND " + res;
+    }
+
+    if(shoutedGreets.length === 1){
+        return res + shoutedGreets[0] + "!";
+    }
+
+    if(shoutedGreets.length === 2){
+        return res +" "+shoutedGreets[0]+" and "+shoutedGreets[1]+"!";
+    }
+
+    for(let i=0; i<shoutedGreets.length-1; i++){
+        res += shoutedGreets[i]+", ";
+    }
+    res += "AND "+shoutedGreets[shoutedGreets-1]+"!";
+    return res;
     
+}
+
+function fillArrays(input) {
+    if(!input){
+        normalGreets.push("my friend");
+        return;
+    }
+    if(Array.isArray(input)){
+        handleArray(input);
+        return;
+    }
+    if(isUpper(input)){
+        shoutedGreets.push(input);
+    }else{
+        normalGreets.push(input);
+    }
+}
+
+function handleArray(arrayOfNames){
+    for (const name of arrayOfNames){
+        if(name.includes('"')){
+            handleNameWithCharacter(name);
+        }else{
+            handleNormal(name);
+        }
+    }
+}
+
+function handleNormal(name){
+    const arr = name.split(",");
+    for(const item of arr){
+        item = item.trim();
+        if(isUpper(item)){
+            shoutedGreets.push(item);
+        }else{
+            normalGreets.push(item);
+        }
+    }
+}
+
+function handleNameWithCharacter(name) {
+    const originalName = name.substring(1,name.length-1);
+    if(isUpper(originalName)){
+        shoutedGreets.push(originalName);
+    }else{
+        normalGreets.push(originalName);
+    }
+}
+
+function isUpper(string){
+    return string === string.toUpperCase();
 }
--- index.test.js
+++ index.test.js
@@ -1,44 +1,44 @@
-const shoutGreet = "HELLO,";
-const normalGreet = "Hello,"
-const shoutTermination = "!";
-const normalTermination = ".";
-
-
-function greet(name = "my friend"){
-    name = name.trim();
-    if(isUpper(name)){
-        return toUpper() + ` ${name}${shoutTermination}`;
-    }
-    return `${normalGreet} ${name}${normalTermination}`;
-}
-
-function isUpper(name){
-    return name === name.toUpperCase();
-}
-
-function toUpper(){
-    return shoutGreet;
-}
+import {greet} from './index.js';
 
 describe("Test Cases for the Greet() Function Loading", () => {
-    it("Should return the passed String with greeting message", () => {
+    it("Should return output as per Requirement1.", () => {
         const input = "Fardeen";
         const greetings = greet(input);
         expect(greetings).toBe("Hello, Fardeen.");
     });
-    it("Should return Hello, My Friend. if message is null", () => {
+    it("Should return output as per Requirement2.", () => {
         // const input = null;
         const greetings = greet();
         expect(greetings).toBe("Hello, my friend.");
     });
-    it("Should return the correct greet message, if message contains trailing spaces or initial spaces", () => {
-        const input = "  Sahil  ";
+    it("Should return output as per Requirement3.", () => {
+        const input = "SAHIL";
         const greetings = greet(input);
-        expect(greetings).toBe("Hello, Sahil.");
+        expect(greetings).toBe("HELLO SAHIL!");
     });
-    it("Should return the HELLO, SAHIL!, if the message is a shouting message", () => {
-        const input = "  SAHIL  ";
+    it("Should return output as per Requirement4.", () => {
+        const input = ["Jill", "Jane"];
         const greetings = greet(input);
-        expect(greetings).toBe("HELLO, SAHIL!");
+        expect(greetings).toBe("Hello, Jill and Jane.");
+    });
+    it("Should return output as per Requirement5.", () => {
+        const input = ["Amy", "Brian", "Charlotte"];
+        const greetings = greet(input);
+        expect(greetings).toBe("Hello, Amy, Brian, and Charlotte.");
+    });
+    it("Should return output as per Requirement6.", () => {
+        const input = ["Amy", "BRIAN", "Charlotte"];
+        const greetings = greet(input);
+        expect(greetings).toBe("Hello, Amy and Charlotte. AND HELLO BRIAN!");
+    });
+    it("Should return output as per Requirement7.", () => {
+        const input = ["Bob", "Charlie, Dianne"];
+        const greetings = greet(input);
+        expect(greetings).toBe("Hello, Bob, Charlie, and Dianne.");
+    });
+    it("Should return output as per Requirement8.", () => {
+        const input = ["Bob", "\"Charlie, Dianne\""];
+        const greetings = greet(input);
+        expect(greetings).toBe("Hello, Bob and Charlie, Dianne.");
     });
 })
