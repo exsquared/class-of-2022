a4caeacb4965076ae20ec50509416fbdf029c970
Lalit Singh <lsingh@EX2INDIA.LOCAL>
2022-04-04 17:43:35 +0530

    commit

--- Program3.cs
+++ Program3.cs
@@ -26,60 +26,26 @@ namespace Project
         {
             string URL = "https://sprint-api.homluv.com/api/v3HomeDetail/p1483576";
             string jsonstring = await FetchJSONData(URL);
-            
+
             RootObject robj = JsonConvert.DeserializeObject<RootObject>(jsonstring);
-            
+
             result.Add("homeId", robj.HomeDetail.homeId);
             result.Add("communityName", robj.HomeDetail.communityName);
             result.Add("homeName", robj.HomeDetail.homeName);
             result.Add("marketName", robj.HomeDetail.marketName);
- 
-            //Dictionary<string, object> finalResult = Iterate(hd);
             Print(result);
         }
-       
+
         protected static async Task<string> FetchJSONData(string URL)
         {
             using (var client = new HttpClient())
             {
                 var response = await client.GetStringAsync(URL);
-                //Console.WriteLine(response);
+
                 return response;
             }
         }
-        //protected static Dictionary<string, object> Iterate(dynamic variable1)
-        //{
-        //    //Dictionary<string, object> result = new Dictionary<string, object>();
-        //    if (variable1.GetType() == typeof(Newtonsoft.Json.Linq.JObject))
-        //    {
-        //        foreach (var property in variable1)
-        //        {
-        //            //Console.WriteLine(property.Name);
-        //            if (property.Name.Equals("homeId"))
-        //            {
-        //                //Console.WriteLine(property.Name + " : " + property.Value);
-        //                result.Add(property.Name, property.Value);
-        //            }
-        //            else if (property.Name.Equals("commName"))
-        //            {
-        //                result.Add("communityName", property.Value);
-        //            }
-        //            else if (property.Name.Equals("planName"))
-        //            {
-        //                result.Add("homeName", property.Value);
-        //            }
-        //            else if (property.Name.Equals("marketName"))
-        //            {
-        //                result.Add(property.Name, property.Value);
-        //            }
-        //            if (property.Value.GetType() == typeof(Newtonsoft.Json.Linq.JObject))
-        //            {
-        //                Iterate(property.Value);
-        //            }
-        //        }
-        //    }
-        //    return result;
-        //}
+
         protected static void Print(Dictionary<string, object> dict)
         {
             foreach (var item in dict)
